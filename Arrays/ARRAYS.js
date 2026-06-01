73465a2d7756c3fefa87f31b5553841b25fb6ece
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-06-01 16:58:37 +0500

    SOLVE ARRAY PROBLEM 2

--- Arrays/ARRAYS.js
+++ Arrays/ARRAYS.js
@@ -2,14 +2,24 @@
 
 // PROBLEM 1
 
-// function printArrayInfo(arr) {
-//     console.log(arr[0])
-//     console.log(arr[arr.length - 1])
-//     console.log(arr.length)
-// }
+function printArrayInfo(arr) {
+    console.log(arr[0])
+    console.log(arr[arr.length - 1])
+    console.log(arr.length)
+}
 
-// const names = ["Ali", "Sara", "Ahmed", "Zara", "Usman"]
+const names = ["Ali", "Sara", "Ahmed", "Zara", "Usman"]
 
-// printArrayInfo(names)
+printArrayInfo(names)
 
+// PROBLEM 2
+
+function printAll(numbers) {
+    for (let number of numbers) {
+        console.log(number)
+    }
+}
+
+const numbers = [1, 2, 3, 4, 5]
+printAll(numbers)
 
