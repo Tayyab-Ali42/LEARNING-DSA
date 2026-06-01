1f8cc60c0f51e9500642c1acda4630084cd65295
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-06-01 17:14:11 +0500

    SOLVE PROBLEM 2 IS STRING PALINDMORE

--- String/String.js
+++ String/String.js
@@ -8,5 +8,22 @@ const reverseString = (str) => {
     return reversedStr
 }
 
-const str = "hello"
-console.log(reverseString(str))
+// const str = "hello"
+// console.log(reverseString(str))
+
+
+// PROBLEM 2
+
+function isPalindrome(str) {
+    let reversedStr = ""
+    for (let index = str.length - 1; index >= 0; index--) {
+        reversedStr += str[index]
+    }
+
+    return reversedStr === str ? true : false
+
+}
+
+// console.log(isPalindrome("hello"))
+// console.log(isPalindrome("racecar"))
+// console.log(isPalindrome("madam"))
