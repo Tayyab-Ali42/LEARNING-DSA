fe6f3c006ba68a66ea515bc8d3f74b7f9e5aa636
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-06-01 16:54:19 +0500

    starts learning DSA

---
+++ Arrays/ARRAYS.js
@@ -0,0 +1,15 @@
+// ARRAYS PROBLEMS
+
+// PROBLEM 1
+
+// function printArrayInfo(arr) {
+//     console.log(arr[0])
+//     console.log(arr[arr.length - 1])
+//     console.log(arr.length)
+// }
+
+// const names = ["Ali", "Sara", "Ahmed", "Zara", "Usman"]
+
+// printArrayInfo(names)
+
+
