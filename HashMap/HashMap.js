d7f08b58b843088d29e4029a21777045adcb3cf1
Tayyab Ali <freelancewithtayyab@gmail.com>
2026-06-02 16:26:01 +0500

    SOLVE PROBLEM 2 USING SET AND MAP FIND DUPLICATES

--- HashMap/HashMap.js
+++ HashMap/HashMap.js
@@ -18,3 +18,26 @@ const TwoSum = (numbers, target) => {
 // const numbers = [2, 7, 11, 15]
 // const target = 9
 // console.log(TwoSum(numbers, target))
+
+
+const findDuplicates = (numbers) => {
+    let seen = new Map()
+    let duplicates = new Set()
+
+    for (const element of numbers) {
+        if (seen.has(element)) {
+            duplicates.add(element)
+        } else {
+            seen.set(element, true)
+        }
+    }
+
+    return [...duplicates]
+
+}
+
+
+
+
+// const numbers = [1, 2, 3, 1, 5, 1, 4, 2, 5, 5]
+// console.log(findDuplicates(numbers))
